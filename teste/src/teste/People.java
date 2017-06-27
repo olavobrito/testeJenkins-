b33c30ce4f1af6921790b488e3fa0631eae0160c
Olavo Brito <olavo.souza@embedded.ufcg.edu.br>
2017-06-27 17:13:55 -0300

    adicionando construtor

--- teste/src/teste/People.java
+++ teste/src/teste/People.java
@@ -1,5 +1,15 @@
 package teste;
 
 public class People {
-
+	
+	String name;
+	int age;
+	char sex;
+	
+	public People(String name, int age, char sex) {
+		this.age = age;
+		this.name = name;
+		this.sex = sex;
+	}
+	
 }
